c38cb52e9566de1224beb11acdba2e133053b706
Devon Bradley <dbradleyfl@gmail.com>
2017-03-27 19:45:42 -0700

    start to implement mrjs commands

--- bin/mrjs.js
+++ bin/mrjs.js
@@ -3,8 +3,24 @@ var shell = require("shelljs");
 
 let args = process.argv.slice(2);
 
-args.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
+// args.forEach(function (val, index, array) {
+//   console.log(index + ': ' + val);
+// });
 
-shell.exec("echo shell.exec works");
+switch (args[0]) {
+  case "new":
+    if (args[1]) {
+      console.log("Creating a mixed reality js app named: ", args[1]);
+    } else {
+      console.log("Please pass an app name after new.");
+    }
+    break;
+  case "start":
+    console.log("Run mrjs app development server.");
+    break;
+  case "package":
+    console.log("Package mrjs app as Universal Windows Platform application.")
+    break;
+  default:
+
+}
